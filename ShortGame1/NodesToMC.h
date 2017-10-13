0e9e8e4c4e45fcce6a7d125a8f6152a9b1e687c0
Asder York <fake14mail@gmail.com>
2017-10-13 21:51:52 +0700

    A mesh is now can be generated! Not rendered, mind you. Just Vertex&Index Vectors. But still!

--- ShortGame1/NodesToMC.h
+++ ShortGame1/NodesToMC.h
@@ -22,13 +22,14 @@ namespace GEM
 
 		/**!
 		We actually wants to find a points on edges of cubes, that would be a part of iso-surface.
-		MidPoint is a representation of thouse points. It's just float pair of (x,y)
-		MidPoint counts us Unset if X or Y is -1; That's their default state
+		MidPoint is a representation of those points. It's just a float triplet of (x,y,z)
+		MidPoint counts as Unset if X or Y or Z is -1; That's their default state
 		*/
 		struct MidPoint {
-			float x, y;
-			MidPoint(float _x, float _y) : x(_x), y(_y) {}
-			MidPoint() : x(-1), y(-1) {}
+			float x, y, z;
+			int VertexVectorPosition = -1;//Position in a VVP this point occupy(Or -1 if it's not in a vector). Used to avoid multiple enterence
+			MidPoint(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
+			MidPoint() : x(-1), y(-1), z(-1) {}
 		};
 
 		/**!
@@ -44,6 +45,11 @@ namespace GEM
 
 		std::vector<std::vector<std::vector<NodeEnvelope>>> NodeEnvelopeVec;
 
+		//Holds pointers to used EdgePoints in order and without duplications
+		std::vector<MidPoint*> VertexVector;
+		//Holds IDs from VertexVector, so that they could be arranged in triangles
+		std::vector<int> IndexVector;
+
 		/**!
 		Acesses node from multiple NodeMap as they are one big NodeMap.
 		\param[in] NodeX Position of a node in a big NodeMap
@@ -58,9 +64,9 @@ namespace GEM
 		*/
 		inline Node& GetNode(int NodeX, int NodeY, int NodeZ)
 		{
-			if ((NodeX > DimXZ))
+			if ((NodeX >= DimXZ))
 			{
-				if ((NodeZ > DimXZ))
+				if ((NodeZ >= DimXZ))
 				{
 					return ChunkFrontRight->NodeMap[NodeX - DimXZ][NodeY][NodeZ - DimXZ];
 				}
@@ -69,13 +75,13 @@ namespace GEM
 					return ChunkRight->NodeMap[NodeX - DimXZ][NodeY][NodeZ];
 				}
 			}
-			else if (NodeZ > DimXZ)
+			else if (NodeZ >= DimXZ)
 			{
 				return ChunkFront->NodeMap[NodeX][NodeY][NodeZ - DimXZ];
 			}
 			else
 			{
-				return ChunkCentre->NodeMap[NodeX][NodeY][NodeZ - DimXZ];
+				return ChunkCentre->NodeMap[NodeX][NodeY][NodeZ];
 			}
 		}
 
@@ -102,7 +108,7 @@ namespace GEM
 			}
 		}
 
-		inline MidPoint& GetMidPoint(int CubeX, int CubeY, int CubeZ, int EdgeID, std::vector<std::vector<std::vector<NodeEnvelope>>> &NodeEnvelopeVec)
+		inline MidPoint& GetMidPoint(int CubeX, int CubeY, int CubeZ, int EdgeID)
 		{
 			/**!
 			So we're trying to do another Map, that contains only EdgePoint, but positionally similar to NodeMap, so that we could use the same trick
@@ -127,8 +133,40 @@ namespace GEM
 			}
 		}
 
+		/**!
+		Calculates a CubeValue for a cubie
+		\param[in] CubeX X-cordinate of a cube
+		\param[in] CubeY Y-cordinate of a cube
+		\param[in] CubeZ Z-cordinate of a cube
+		\returns CubeValue of a Cubie
+		*/
+		inline int GetCubieValue(int CubeX, int CubeY, int CubeZ)
+		{
+			int cubeindex = 0;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 0).Value > 0) cubeindex |= 1;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 1).Value > 0) cubeindex |= 2;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 2).Value > 0) cubeindex |= 4;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 3).Value > 0) cubeindex |= 8;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 4).Value > 0) cubeindex |= 16;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 5).Value > 0) cubeindex |= 32;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 6).Value > 0) cubeindex |= 64;
+			if (GetNodeAsCubie(CubeX, CubeY, CubeZ, 7).Value > 0) cubeindex |= 128;
+			return cubeindex;
+		}
 
-
+		/**!
+		Calculates MidPoint on a specified Edge
+		\param[in] CubeX Cordinate of a cube
+		\param[in] CubeY Cordinate of a cube
+		\param[in] CubeZ Cordinate of a cube
+		\param[in] EdgeID ID of edge, which MidPoint must be calculated
+		*/
+		MidPoint& CalcMidPoint(int CubeX, int CubeY, int CubeZ, int EdgeID);
+		
+		/**!
+		Processes once Cube. Calculates its value, sets midpoints, creates triangles
+		*/
+		void ProcessCube(int CubeX, int CubeY, int CubeZ);
 
 	public:
 		NodesToMCGenerator(std::shared_ptr<GEM::NodeChunk> _ChunkCentre, std::shared_ptr<GEM::NodeChunk> _ChunkRight, std::shared_ptr<GEM::NodeChunk> _ChunkFront, std::shared_ptr<GEM::NodeChunk> _ChunkCentreFront, int _DimXZ, int _DimY)
